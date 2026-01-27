b7d6f29cbb7298083cf8fc3aa5483a3434e45425
Hasnainali404 <jeinn467@gmail.com>
2026-01-27 06:10:21 -0800

    first commit 01

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/newsapi': {
-        target: 'https://newsapi.org',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/newsapi/, ''),
-      },
-    },
-  },
 })
